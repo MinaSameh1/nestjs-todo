bebf663a115b72eac0f01416f9ffe92fae59be04
MinaSameh1 <minasameh1@gmail.com>
2022-06-10 00:09:06 +0200

    Basic Structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, TodoModule],
 })
 export class AppModule {}
