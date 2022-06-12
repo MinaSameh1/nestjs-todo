8453c9b125e6cd803cd43dff118f3baba564ac04
MinaSameh1 <minasameh1@gmail.com>
2022-06-13 01:16:30 +0200

    simple refactor

--- src/common/decorators/get_current_user.decorator.ts
+++ src/common/decorators/get_current_user.decorator.ts
@@ -3,8 +3,8 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 export const GetCurrentUser = createParamDecorator(
   (data: string | undefined, context: ExecutionContext) => {
     const req = context.switchToHttp().getRequest()
-    if (!data) return req.user
-    return req.user[data]
+    if (data) return req.user[data]
+    return req.user
   }
 )
 
