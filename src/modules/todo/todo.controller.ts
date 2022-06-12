a199ffa2171242d0b37c3fd5ddf8f97f42a86b16
MinaSameh1 <minasameh1@gmail.com>
2022-06-12 18:04:22 +0200

    Finished Basic logic of todo controller

--- src/modules/todo/todo.controller.ts
+++ src/modules/todo/todo.controller.ts
@@ -1,9 +1,30 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post } from "@nestjs/common";
+import { GetCurrentUserId } from "src/common/decorators";
 import { ToDoService } from "./todo.service";
+import { ToDoCreateDTO } from './dto'
 
 @Controller('todo')
 export class ToDoController {
-  constructor(private todoService: ToDoService) {}
+  constructor(private todoService: ToDoService) { }
 
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getTodos(@GetCurrentUserId() userId: number) {
+    return this.getTodos(userId)
+  }
 
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async createTodo(
+    @GetCurrentUserId() userId: number,
+    @Body() input: ToDoCreateDTO
+  ) {
+    return await this.todoService.createToDo(input, userId)
+  }
+
+  @Delete(':toDoId')
+  @HttpCode(HttpStatus.OK)
+  async deleteTodo(@Param('toDoId') toDoId) {
+    return await this.todoService.deleteToDo(toDoId)
+  }
 }
--- src/modules/todo/todo.service.ts
+++ src/modules/todo/todo.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from "@nestjs/common"
 import { PrismaService } from "../prisma/prisma.service";
+import { ToDoCreateDTO } from './dto'
 
 @Injectable()
 export class ToDoService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
+  getTodo(userId: number) {
+    return this.prisma.toDo.findMany({
+      where: {
+        userId: userId
+      }
+    })
+  }
+
+  createToDo(input: ToDoCreateDTO, userId: number) {
+    return this.prisma.toDo.create({
+      data: {
+        ...input,
+        userId: userId
+      }
+    })
+  }
+
+  deleteToDo(toDoId: number) {
+    return this.prisma.toDo.delete({
+      where: {
+        id: toDoId
+      }
+    })
+  }
 }
